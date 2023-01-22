f6cae9ca70df0124957ecee5ae513b0edcc721c9
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-01-21 21:53:21 -0500

    GMJ- le timer reste à 0 lorsque le  countDown est fini

--- client/src/app/components/timer/timer.component.ts
+++ client/src/app/components/timer/timer.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
-@Input() time : number;
+@Input() countDown : number = 0; // the time must be transformed to second before being processed
 second: number = 0;
 minute: number = 0;
 count: number = 0;
@@ -14,27 +14,39 @@ count: number = 0;
   constructor() { }
 
   ngOnInit(): void {
-    this.startTimer();
+    this.startCountDown();
   }
 
   startTimer(): number{
     setInterval(() => {
       this.count++;
-      console.log(this.count);
     }, 1000);
     return this.count;
   }
 
   startCountDown():number{
     setInterval(() => {
-      this.time--;
-      console.log(this.time);
+      this.countDown--;
     }, 1000);
-    return this.time;
+    return this.countDown;
+  }
+  stopCountDown():void{
+    this.countDown = 0;
   }
 
   formatTime(): string{
     return (this.minute < 10 ? "0" + this.minute : this.minute) + ":" + (this.second < 10 ? "0" + this.second : this.second);
+  }
+
+  transformCountDown (): string{
+    this.second = this.countDown % 60;
+    this.minute = Math.floor(this.countDown / 60);
+    if(this.second<=0 && this.minute<=0){
+      return "00:00";
+    }
+    else{
+      return (this.second < 10 || this.minute < 10) ? this.formatTime() : this.minute + ":" + this.second;
+    }
   }
 
   transform (): string{
@@ -43,8 +55,9 @@ count: number = 0;
     return (this.second < 10 || this.minute < 10) ? this.formatTime() : this.minute + ":" + this.second;
   }
 
-  stopTimer():void {
-    console.log("stop");
+  stopTimer(): number{
+    //return this.count;
+    return 0;
   }
 
 }

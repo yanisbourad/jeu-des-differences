819afebdfdb7c77bb1b90bff8e71f7b6967dd41c
Hamza Berrada <hamza.berrada@bell.ca>
2023-01-22 14:55:20 -0500

    add option to change color or lineWidth of drawing

--- client/src/app/services/draw.service.ts
+++ client/src/app/services/draw.service.ts
@@ -11,6 +11,8 @@ export const DEFAULT_HEIGHT = 480;
 export class DrawService {
     context: CanvasRenderingContext2D;
     private canvasSize: Point = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
+    private color: string = 'black';
+    private lineWidth: number = 3;
 
     get width(): number {
         return this.canvasSize.x;
@@ -18,6 +20,14 @@ export class DrawService {
 
     get height(): number {
         return this.canvasSize.y;
+    }
+
+    setColor(color: string) {
+        this.color = color;
+    }
+
+    setLineWidth(width: number) {
+        this.lineWidth = width;
     }
 
     drawImage(image: ImageBitmap, canvas: HTMLCanvasElement) {
@@ -30,11 +40,15 @@ export class DrawService {
         this.context.beginPath();
         this.context.moveTo(lastPoint.x, lastPoint.y);
         this.context.lineTo(point.x, point.y);
-        this.context.strokeStyle = 'blue';
-        this.context.lineWidth = 2;
+        this.context.strokeStyle = this.color;
+        this.context.lineWidth = this.lineWidth;
         this.context.stroke();
     }
     // drawLine(linePoints: Vec2[], canvas: HTMLCanvasElement) {}
+
+    // drowCube(cubePoints: Vec2[], canvas: HTMLCanvasElement) {}
+
+    // drawTriangle(canvas: HTMLCanvasElement) {}
 
     // clear(canvas: HTMLCanvasElement) {}
 }

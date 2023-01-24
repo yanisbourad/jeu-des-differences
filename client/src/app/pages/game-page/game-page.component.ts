9b0a0d3ecc1962d571777b4f987b0c03ca351581
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-01-24 16:14:09 -0500

    GMJ- implementation vue du jeu, work in progress

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -6,13 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-page.component.scss']
 })
 export class GamePageComponent implements OnInit {
-
-  constructor() { }
+  readonly DEFAULT_WIDTH = 640;
+  readonly DEFAULT_HEIGHT = 480;
+  gameName : string;
+  gameInfos : string;
+  differences : number;
+  //in infos component change display depending of the game mode (solo, multijoueur, temps limite)
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
 
   giveUp():void{
+    //feedback message : {Êtes-vous sur de vouloir abandonner la partie? Cette action est irréversible.}
+    //if yes do 
+        //stop game 
+        //save infos???
+        //redirect user to main page
+    //else close modal and continue the game
 
   }
 }

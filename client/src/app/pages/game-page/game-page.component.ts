ec7422ac38ab4d30c19cf178bbb4b0d17ea249bb
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-01-26 15:38:46 -0500

    GMJ - logic added for differences

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { imagePath } from '@app/interfaces/imageDiffPath';
+import { TimeService } from '@app/services/time.service';
 
 @Component({
   selector: 'app-game-page',
@@ -8,16 +10,42 @@ import { Component, OnInit } from '@angular/core';
 export class GamePageComponent implements OnInit {
   readonly DEFAULT_WIDTH = 640;
   readonly DEFAULT_HEIGHT = 480;
+  path: imagePath = {
+    path1: '../../../assets/img/differenceEye.png',
+    path2: '../../../assets/img/eyeFound.png',
+  }
+  
   gameName : string;
   gameInfos : string;
-  differences : number;
-  //in infos component change display depending of the game mode (solo, multijoueur, temps limite)
-  constructor() { 
-  }
+  nbrdifferences : number =8;
+  differences : string[] = new Array(this.nbrdifferences);
+  differencesFound : number = 0;
+  isFound: boolean = false;
 
+
+  //in infos component change display depending of the game mode (solo, multijoueur, temps limite)
+  constructor( private readonly timeService: TimeService) {
+    this.generateImage();
+  }
   ngOnInit(): void {
   }
-
+  generateImage(): void{
+    //generate image
+    for (let i = 0; i < this.nbrdifferences; i++){
+      this.differences[i] = this.path.path1;
+    }
+    
+  }
+  clicked(): void{
+    if (this.differencesFound < this.nbrdifferences){
+      this.differencesFound++;
+      this.differences.pop();
+      this.differences.unshift(this.path.path2);
+    }
+    if(this.differencesFound === this.nbrdifferences){
+      this.timeService.stopTimer();
+    }
+  }
   giveUp():void{
     //feedback message : {Êtes-vous sur de vouloir abandonner la partie? Cette action est irréversible.}
     //if yes do 
@@ -27,4 +55,5 @@ export class GamePageComponent implements OnInit {
     //else close modal and continue the game
 
   }
+
 }

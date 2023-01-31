7c93aa73f7c39c144905623d97498b44154e35a4
relhe <renellherisson@gmail.com>
2023-01-31 10:53:15 -0500

    rl: image processing function completed after create button clicked

---
+++ client/src/app/components/validate-create-btn/validate-create-btn.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { CanvasHolderService } from '@app/services/canvas-holder.service';
+import { ImageDiffService } from '@app/services/image-diff.service';
+
+@Component({
+    selector: 'app-validate-create-btn',
+    templateUrl: './validate-create-btn.component.html',
+    styleUrls: ['./validate-create-btn.component.scss'],
+})
+export class ValidateCreateBtnComponent implements OnInit {
+    @Input() text!: string;
+    @Input() color!: string;
+    // eslint-disable-next-line @angular-eslint/no-output-on-prefix
+    @Output() onBtnClick = new EventEmitter();
+
+    constructor(private readonly canvasHolder: CanvasHolderService, private readonly imageDifferenceService: ImageDiffService) {}
+
+    ngOnInit(): void {}
+
+    onClick() {
+        const originalData = this.canvasHolder.getCanvasData(this.canvasHolder.originalCanvas);
+        const modifiedData = this.canvasHolder.getCanvasData(this.canvasHolder.modifiedCanvas);
+        if (originalData && modifiedData) {
+            this.imageDifferenceService.setPixelMatrix(originalData, modifiedData);
+            this.imageDifferenceService.getDifferenceMatrix();
+            this.imageDifferenceService.setDifferenceDataToDraw();
+            this.imageDifferenceService.defineDifferences();
+        } else {
+            alert('Error: No image loaded!');
+        }
+        this.onBtnClick.emit();
+    }
+}

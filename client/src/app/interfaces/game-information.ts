76bccd91d8c4d80db271a6f6a526f85fb785e261
Daniel Giao <daniel.giao@icloud.com>
2023-02-01 08:08:20 -0500

    DG - Icons refactor and added

---
+++ client/src/app/interfaces/game-information.ts
@@ -0,0 +1,6 @@
+export interface GameInformation {
+    gameTitle: string;
+    gameMode: 'Partie Classique en mode solo' | 'Partie Classique en mode 1 vs 1';
+    nDifferences: number;
+    nHints: number;
+}

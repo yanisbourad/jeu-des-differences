19784eb2939bc7a8228674884ff6495c06e90df6
Daniel Giao <daniel.giao@icloud.com>
2023-02-01 09:01:54 -0500

    DG - Message disabled inputs

--- client/src/app/components/message-area/message-area.component.ts
+++ client/src/app/components/message-area/message-area.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./message-area.component.scss'],
 })
 export class MessageAreaComponent implements OnInit {
+    playerName: string = 'Daniel';
+    eventMessageArray: string[] = new Array();
+
     constructor() {}
     ngOnInit(): void {}
 }

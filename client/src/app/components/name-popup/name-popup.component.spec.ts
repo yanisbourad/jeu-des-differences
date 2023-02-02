76c45e60dd340da4222d80e6bca8e1bad0516b93
Hamza Berrada <hamza.berrada@bell.ca>
2023-02-01 20:47:54 -0500

    Configure routing for Config page page and game create page

--- client/src/app/components/name-popup/name-popup.component.spec.ts
+++ client/src/app/components/name-popup/name-popup.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NamePopupComponent } from './name-popup.component';
 
 describe('NamePopupComponent', () => {
-  let component: NamePopupComponent;
-  let fixture: ComponentFixture<NamePopupComponent>;
+    let component: NamePopupComponent;
+    let fixture: ComponentFixture<NamePopupComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NamePopupComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NamePopupComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(NamePopupComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(NamePopupComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

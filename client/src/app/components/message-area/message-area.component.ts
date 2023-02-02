ef21809764b9104f702d3983eac2686cfcc81154
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-02-01 19:01:41 -0500

    GMJ-scroll bar added

--- client/src/app/components/message-area/message-area.component.ts
+++ client/src/app/components/message-area/message-area.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MessageAreaComponent implements OnInit {
     playerName: string = 'Daniel';
     eventMessageArray: string[] = new Array();
-
+    test:number[] = new Array(25)
     constructor() {}
     ngOnInit(): void {}
 }

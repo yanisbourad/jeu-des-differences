b45bb1a5ac8e5771d20cf082e88ee566de350fe1
relhe <renellherisson@gmail.com>
2023-02-07 13:44:34 -0500

    definition of single responsabilite for create function

--- client/src/app/components/validate-create-btn/validate-create-btn.component.ts
+++ client/src/app/components/validate-create-btn/validate-create-btn.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CanvasHolderService } from '@app/services/canvas-holder.service';
-import { GameDatabaseService } from '@app/services/game-database.sercice';
 import { ImageDiffService } from '@app/services/image-diff.service';
 
 @Component({
@@ -14,11 +13,7 @@ export class ValidateCreateBtnComponent {
     // eslint-disable-next-line @angular-eslint/no-output-on-prefix
     @Output() onBtnClick = new EventEmitter();
 
-    constructor(
-        private readonly canvasHolder: CanvasHolderService,
-        private readonly imageDifferenceService: ImageDiffService,
-        private readonly gameDataBase: GameDatabaseService,
-    ) {}
+    constructor(private readonly canvasHolder: CanvasHolderService, private readonly imageDifferenceService: ImageDiffService) {}
 
     onClick() {
         const originalData = this.canvasHolder.getCanvasData(this.canvasHolder.originalCanvas);
@@ -28,10 +23,6 @@ export class ValidateCreateBtnComponent {
             this.imageDifferenceService.setPixelMatrix(originalData, modifiedData);
             this.imageDifferenceService.setDifferenceDataToDraw();
             this.imageDifferenceService.defineDifferences();
-            const text = 'Enter the name of the game:';
-            const defaultText = 'Game name';
-            const resp = window.prompt(text, defaultText);
-            this.gameDataBase.saveGame(resp?.toString() || 'hiihi');
         } else {
             alert('Error: No image loaded!');
         }

36dcb398764daf6fc1f590ed5384e76c2b1e6e70
relhe <renellherisson@gmail.com>
2023-02-07 14:12:08 -0500

    create form and add new function to run for cancel and submit button when clicked

--- client/src/app/components/game-name-save/game-name-save.component.spec.ts
+++ client/src/app/components/game-name-save/game-name-save.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GameNameSaveComponent } from './game-name-save.component';
 
 describe('GameNameSaveComponent', () => {
-  let component: GameNameSaveComponent;
-  let fixture: ComponentFixture<GameNameSaveComponent>;
+    let component: GameNameSaveComponent;
+    let fixture: ComponentFixture<GameNameSaveComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameNameSaveComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [GameNameSaveComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(GameNameSaveComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(GameNameSaveComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/components/game-name-save/game-name-save.component.ts
+++ client/src/app/components/game-name-save/game-name-save.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { GameDatabaseService } from '@app/services/game-database.sercice';
 
 @Component({
-  selector: 'app-game-name-save',
-  templateUrl: './game-name-save.component.html',
-  styleUrls: ['./game-name-save.component.scss']
+    selector: 'app-game-name-save',
+    templateUrl: './game-name-save.component.html',
+    styleUrls: ['./game-name-save.component.scss'],
 })
-export class GameNameSaveComponent implements OnInit {
+export class GameNameSaveComponent {
+    gameName: string;
+    level: string;
+    constructor(public dialogRef: MatDialogRef<GameNameSaveComponent>, private readonly gameDataBase: GameDatabaseService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    getGameData() {
+        this.gameDataBase.saveGame(this.gameName);
+        this.dialogRef.close();
+    }
+    closeOnAbort() {
+        this.dialogRef.close();
+    }
 }

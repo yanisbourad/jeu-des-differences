32505cdf9bfdcf70bfb09d5ba7d325c8342e7a85
Daniel Giao <daniel.giao@icloud.com>
2023-02-07 23:11:09 -0500

    DG - Added image-diff service import dependencies

---
+++ client/src/app/configuration/const-canvas.ts
@@ -0,0 +1,14 @@
+// this file contains all the constants used in the canvas
+export const defaultWidth = 640;
+export const defaultHeight = 480;
+export const defaultLineWidth = 5;
+export const minLineWidth = 1;
+export const maxLineWidth = 10;
+export const defaultRadius = 3;
+export const defaultLineColor = '#000000';
+export const defaultBackgroundColor = '#ffffff';
+// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+export const possibleRadius = [0, 3, 6, 9, 15];
+export const nextPixelStartIndex = 4;
+export const incrementValue = 1;
+export const desiredBitDepth = 24;

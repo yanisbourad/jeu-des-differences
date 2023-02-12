3a9d41ba4ea460b35ac0161e0c3d9be4e9c8db15
Hamza Berrada <hamza.berrada@bell.ca>
2023-02-12 14:13:37 -0500

    correct errors in tests

--- client/src/app/pages/game-creation-page/game-creation-page.component.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.ts
@@ -10,9 +10,9 @@ import { CanvasHolderService } from '@app/services/canvas-holder.service';
     styleUrls: ['./game-creation-page.component.scss'],
 })
 export class GameCreationPageComponent implements OnInit {
-    @ViewChild('originalCanvasComponent') private originalCanvasComponent: CanvasNgxComponent;
-    @ViewChild('modifiedCanvasComponent', { static: false }) private modifiedCanvasComponent: CanvasNgxComponent;
-    @ViewChild('fileUpload', { static: false }) private fileUpload!: ElementRef<HTMLInputElement>;
+    @ViewChild('originalCanvasComponent') originalCanvasComponent: CanvasNgxComponent;
+    @ViewChild('modifiedCanvasComponent') modifiedCanvasComponent: CanvasNgxComponent;
+    @ViewChild('fileUpload', { static: false }) fileUpload!: ElementRef<HTMLInputElement>;
     reposition: boolean = false;
 
     constructor(
@@ -32,9 +32,7 @@ export class GameCreationPageComponent implements OnInit {
     ngOnInit(): void {
         this.canvasHolderService.clearCanvas();
     }
-    goBack(): void {
-        this.canvasHolderService.clearCanvas();
-    }
+
     handleSwapping(element1Id: string, element2Id: string): void {
         const element1Image = this.element.nativeElement.querySelector(element1Id);
         const middleButtons = this.element.nativeElement.querySelector('.swapping-canvas');

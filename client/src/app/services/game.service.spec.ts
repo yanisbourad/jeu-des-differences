978a824439c7c311cc566048d1d080cb07f4cf3b
Daniel Giao <daniel.giao@icloud.com>
2023-02-13 16:46:03 -0500

    DG - DisplayIcons and defineVariables test pass

--- client/src/app/services/game.service.spec.ts
+++ client/src/app/services/game.service.spec.ts
@@ -1,16 +1,16 @@
 import { RendererFactory2 } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { GameInformation } from '@app/interfaces/game-information';
-// import { ImagePath } from '@app/interfaces/hint-diff-path';
-import { SocketClientService } from './socket-client.service';
-import { Game, GameInfo } from '@common/game';
-import { GameDatabaseService } from './game-database.service';
-import { ClientTimeService } from './client-time.service';
-import { GameService } from './game.service';
-import SpyObj = jasmine.SpyObj;
 import { MessageDialogComponent } from '@app/components/message-dialog/message-dialog.component';
+import { GameInformation } from '@app/interfaces/game-information';
+import { ImagePath } from '@app/interfaces/hint-diff-path';
+import { Game, GameInfo } from '@common/game';
 import { of } from 'rxjs/internal/observable/of';
+import { ClientTimeService } from './client-time.service';
+import { GameDatabaseService } from './game-database.service';
+import { GameService } from './game.service';
+import { SocketClientService } from './socket-client.service';
+import SpyObj = jasmine.SpyObj;
 
 describe('GameService', () => {
     let rendererFactory2Spy: SpyObj<RendererFactory2>;
@@ -19,17 +19,17 @@ describe('GameService', () => {
     let gameDataBaseSpy: SpyObj<GameDatabaseService>;
     let socketClientServiceSpy: SpyObj<SocketClientService>;
     let gameService: GameService;
-    // let path: ImagePath;
+    let path: ImagePath;
     let game: Game;
     let gameInformation: GameInformation;
     let gameInfo: GameInfo;
-    let nDifferencesNotFound: number;
+    // let nDifferencesNotFound: number;
     // let nDifferencesFound: number;
-    let differencesArray: string[];
+    // let differencesArray: string[];
     // let isGameFinished: boolean;
-    let nHintsUnused: number;
+    // let nHintsUnused: number;
     // let nHintsUsed: number;
-    let hintsArray: string[];
+    // let hintsArray: string[];
     // let playerName: string;
 
     beforeEach(() => {
@@ -68,11 +68,10 @@ describe('GameService', () => {
             gameMode: 'solo',
             gameDifficulty: 'easy',
             nDifferences: 3,
-            nHints: 0,
+            nHints: 3,
             hintsPenalty: 0,
             isClassical: false,
         };
-
         gameInfo = {
             gameName: 'Test Game',
             difficulty: 'easy',
@@ -81,6 +80,12 @@ describe('GameService', () => {
             listDifferences: ['1', '2', '3'],
             rankingMulti: [],
             rankingSolo: [],
+        };
+        path = {
+            differenceNotFound: 'differenceNotFound.png',
+            differenceFound: 'differenceFound.png',
+            hintUnused: 'hintUnused.png',
+            hintUsed: 'hintUsed.png',
         };
     });
 
@@ -91,18 +96,20 @@ describe('GameService', () => {
     it('should call defineVariables', () => {
         gameService.game = game;
         gameService.gameInformation = gameInformation;
+        const nDifferencesNotFound = 3;
+        const nHintsUnused = 3;
         gameService.defineVariables();
         expect(gameService.gameInformation.gameTitle).toBe(game.gameName);
         expect(gameService.gameInformation.gameMode).toBe('solo');
         expect(gameService.gameInformation.gameDifficulty).toBe(game.difficulty);
         expect(gameService.gameInformation.nDifferences).toBe(game.listDifferences.length);
-        expect(gameService.gameInformation.nHints).toBe(0);
-        expect(gameService.gameInformation.hintsPenalty).toBe(0);
+        expect(gameService.gameInformation.nHints).toBe(3);
+        expect(gameService.gameInformation.hintsPenalty).toBe(5);
         expect(gameService.gameInformation.isClassical).toBe(false);
-        expect(nDifferencesNotFound).toBe(3);
-        expect(nHintsUnused).toBe(0);
-        expect(differencesArray.length).toBe(3);
-        expect(hintsArray.length).toBe(0);
+        expect(gameService.nDifferencesNotFound).toBe(gameInformation.nDifferences);
+        expect(gameService.nHintsUnused).toBe(gameInformation.nHints);
+        expect(gameService.differencesArray.length).toEqual(nDifferencesNotFound);
+        expect(gameService.hintsArray.length).toEqual(nHintsUnused);
     });
 
     it('getGame should retrieve the game from the server and call defineVariables', () => {
@@ -117,4 +124,23 @@ describe('GameService', () => {
         expect(gameDataBaseSpy.getGameByName).toHaveBeenCalled();
         expect(defineVariablesSpy).toHaveBeenCalled();
     });
+
+    it('displayIcons should fill the array of differences and hints', () => {
+        gameService.nDifferencesNotFound = 3;
+        gameService.nHintsUnused = 1;
+        gameService.path = path;
+        gameService.differencesArray = new Array(3);
+        gameService.hintsArray = new Array(1);
+        gameService.displayIcons();
+        for (let i = 0; i < gameService.nDifferencesNotFound; i++) {
+            expect(gameService.differencesArray[i]).toEqual(gameService.path.differenceNotFound);
+        }
+        for (let i = 0; i < gameService.nHintsUnused; i++) {
+            expect(gameService.hintsArray[i]).toEqual(gameService.path.hintUnused);
+        }
+        expect(gameService.differencesArray.length).toBe(3);
+        expect(gameService.hintsArray.length).toBe(1);
+    });
+
+
 });

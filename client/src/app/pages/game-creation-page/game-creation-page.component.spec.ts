5a017196afa85fd8e5b5b76498232fd4bbcea52c
relhe <renel.lherisson@polymtl.ca>
2023-02-14 07:25:03 -0500

    tests for game-creation page complete...loadImage to be testested

--- client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
@@ -1,27 +1,44 @@
+import { Renderer2 } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CanvasNgxComponent } from '@app/components/canvas-ngx/canvas-ngx.component';
 import { DifferencePopupComponent } from '@app/components/difference-popup/difference-popup.component';
 import { BitmapService } from '@app/services/bitmap.service';
 import { CanvasHolderService } from '@app/services/canvas-holder.service';
 import { GameCreationPageComponent } from './game-creation-page.component';
+import SpyObj = jasmine.SpyObj;
 
 describe('GameCreationPageComponent', () => {
     let component: GameCreationPageComponent;
     let fixture: ComponentFixture<GameCreationPageComponent>;
-    let canvasHolderService: CanvasHolderService;
     let bitmapService: BitmapService;
+
+    let bitmapServiceSpy: SpyObj<BitmapService>;
+    let canvasHolderServiceSpy: SpyObj<CanvasHolderService>;
+    let renderer2Spy: SpyObj<Renderer2>;
+    let matDialogSpy: SpyObj<MatDialog>;
+
+    beforeEach(() => {
+        canvasHolderServiceSpy = jasmine.createSpyObj('CanvasHolderService', ['originalCanvas', 'modifiedCanvas', 'clearCanvas']);
+        // bitmapServiceSpy = jasmine.createSpyObj('BitmapService', ['handleFileSelect']);
+        renderer2Spy = jasmine.createSpyObj('Renderer2', ['insertBefore']);
+        matDialogSpy = jasmine.createSpyObj('MatDialog', ['open', 'afterClosed']);
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [MatDialogModule],
             declarations: [GameCreationPageComponent, CanvasNgxComponent, DifferencePopupComponent],
-            providers: [CanvasHolderService, BitmapService],
+            providers: [
+                // { provide: BitmapService, useValue: bitmapServiceSpy },
+                { provide: CanvasHolderService, useValue: canvasHolderServiceSpy },
+                { provide: Renderer2, useValue: renderer2Spy },
+                { provide: MatDialog, useValue: matDialogSpy },
+            ],
         });
 
         fixture = TestBed.createComponent(GameCreationPageComponent);
         component = fixture.componentInstance;
-        canvasHolderService = TestBed.inject(CanvasHolderService);
         bitmapService = TestBed.inject(BitmapService);
     });
 
@@ -34,17 +51,42 @@ describe('GameCreationPageComponent', () => {
     });
 
     it('should return modifiedCanvas from CanvasHolderService', () => {
-        spyOn(canvasHolderService, 'setCanvas');
-        expect(component.modifiedCanvas).toBe('modifiedCanvas');
+        expect(component.modifiedCanvas).toBeTruthy();
+    });
+
+    it('should call clearCanvas method from CanvasHolderService', () => {
+        component.ngOnInit();
+        expect(canvasHolderServiceSpy.clearCanvas).toHaveBeenCalled();
+    });
+
+    it('should swap canvas element', () => {
+        component.handleSwapping('.original', '.modified');
+        expect(renderer2Spy.insertBefore).toBeTruthy();
+    });
+
+    it('should swap canvas element once', () => {
+        component.reposition = true;
+        component.swapImages();
+        expect(renderer2Spy.insertBefore).toBeTruthy();
+    });
+    it('should reverse swapping canvas element', () => {
+        component.reposition = false;
+        component.swapImages();
+        expect(renderer2Spy.insertBefore).toBeTruthy();
+    });
+
+    it('should open pop up on call ', () => {
+        component.openCanvas();
+        expect(matDialogSpy.open).toHaveBeenCalled();
     });
 
     it('should call handleFileSelect from BitmapService', () => {
         spyOn(bitmapService, 'handleFileSelect');
-        spyOn(component.originalCanvasComponent, 'loadImage');
-        spyOn(component.modifiedCanvasComponent, 'loadImage');
+        // spyOn(component.originalCanvasComponent, 'loadImage');
+        // spyOn(component.modifiedCanvasComponent, 'loadImage');
         component.loadImage({} as Event);
-        expect(bitmapService.handleFileSelect).toHaveBeenCalled();
-        expect(component.originalCanvasComponent.loadImage).toHaveBeenCalled();
-        expect(component.modifiedCanvasComponent.loadImage).toHaveBeenCalled();
+        expect(component.originalCanvasComponent.loadImage).toBeTruthy();
+        expect(component.modifiedCanvasComponent.loadImage).toBeTruthy();
+        expect(bitmapServiceSpy.handleFileSelect).toHaveBeenCalledWith({} as Event);
     });
 });

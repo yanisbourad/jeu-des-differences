454d70423641358bb6f13c713d42180b4d0c132d
yanis bourad <yanis.bourad@polymt.ca>
2023-02-14 11:43:24 -0500

    YB: game info and header tests done

--- client/src/app/components/game-info/game-info.component.spec.ts
+++ client/src/app/components/game-info/game-info.component.spec.ts
@@ -1,25 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { GameInformation } from '@app/interfaces/game-information';
+import * as constants from '@app/configuration/const-game';
 import { GameService } from '@app/services/game.service';
 import { GameInfoComponent } from './game-info.component';
 import SpyObj = jasmine.SpyObj;
 
+const mockCards: GameInformation = {
+    gameTitle: 'titre 1',
+    gameMode: 'solo',
+    gameDifficulty: 'facile',
+    nDifferences: 5,
+    nHints: 0,
+    hintsPenalty: 0,
+    isClassical: true,
+};
 describe('GameInfoComponent', () => {
-    let gameServiceSpy: SpyObj<GameService>;
     let component: GameInfoComponent;
     let fixture: ComponentFixture<GameInfoComponent>;
-
-    beforeEach(() => {
-        gameServiceSpy = jasmine.createSpyObj('GameService', ['displayIcons']);
-    });
+    let gameServiceSpy: SpyObj<GameService>;
 
     beforeEach(async () => {
-        await TestBed.configureTestingModule({
+        gameServiceSpy = jasmine.createSpyObj('GameService', ['displayIcons']);
+        gameServiceSpy.gameInformation = mockCards;
+        TestBed.configureTestingModule({
             declarations: [GameInfoComponent],
             providers: [{ provide: GameService, useValue: gameServiceSpy }],
         }).compileComponents();
-    });
-
-    beforeEach(() => {
         fixture = TestBed.createComponent(GameInfoComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -29,9 +35,9 @@ describe('GameInfoComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('ngOnInit should call displayIcons from gameService', () => {
+    it('ngOnInit should call displayIcons from gameService', fakeAsync(() => {
         component.ngOnInit();
-        expect(component.ngOnInit()).toHaveBeenCalled();
+        tick(constants.WAITING_TIME);
         expect(gameServiceSpy.displayIcons).toHaveBeenCalled();
-    });
+    }));
 });

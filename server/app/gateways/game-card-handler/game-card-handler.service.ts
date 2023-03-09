6ecda01f57275d4ee86d10dea38f67b5449c65f3
relhe <renellherisson@gmail.com>
2023-03-08 21:12:08 -0500

    completed service methods for game card handler

--- server/app/gateways/game-card-handler/game-card-handler.service.ts
+++ server/app/gateways/game-card-handler/game-card-handler.service.ts
@@ -1,36 +1,85 @@
 import { Injectable } from '@nestjs/common';
-import { CreateGameCardHandlerDto } from './dto/create-game-card-handler.dto';
-
+import { Player } from './entities/player.entity';
 @Injectable()
 export class GameCardHandlerService {
-    gamesQueue = {};
-    gamers = {};
+    gamesQueue: Map<string, string[]>;
+    players: Map<string, Player>;
 
-    async create(createGameCardHandlerDto: CreateGameCardHandlerDto) {
-        const updatedGameInfo = { gameName: createGameCardHandlerDto.gameName, isCreated: !createGameCardHandlerDto.isCreated };
-        if (!this.gamesQueue[createGameCardHandlerDto.gameName]) this.gamesQueue[createGameCardHandlerDto.gameName] = [];
-        return updatedGameInfo;
+    constructor() {
+        this.gamesQueue = new Map();
+        this.players = new Map();
     }
 
-    getGamerName(gamerID: string): string {
-        return this.gamers[gamerID];
+    findAllGamesStatus() {
+        if (this.gamesQueue.size === 0) return {};
+        const gamesStatus = {};
+        this.gamesQueue.forEach((value, key) => {
+            gamesStatus[key] = value.length;
+        });
+        return gamesStatus;
     }
 
-    identify(name: string, id: string): string {
-        this.gamers[id] = name;
-        return name;
+    // true if not empty false if it is
+    isPlayerWaiting(player: Player): boolean {
+        if (this.gamesQueue.has(player.gameName)) {
+            if (this.gamesQueue.get(player.gameName).length === 0) return false;
+            return true;
+        } else {
+            this.gamesQueue[player.gameName] = [];
+            return false;
+        }
     }
 
-    acceptOpponent(id: string) {
-        return `This action returns a #${id} gameCardHandler`;
+    // add player at the stack fifo
+    stackPlayer(player: Player): number {
+        this.players.set(player.id, player);
+        if (!this.isPlayerWaiting(player)) {
+            this.gamesQueue.get(player.gameName).push(player.id);
+            return 1;
+        } else {
+            return this.dispatchPlayer(player);
+        }
     }
 
-    sendFeedback(id: string): boolean {
+    // add second player at the stack fifo
+    dispatchPlayer(player: Player): number {
+        if (this.gamesQueue.has(player.gameName)) {
+            this.gamesQueue.get(player.gameName).push(player.id);
+            return 2;
+        }
+    }
+
+    acceptOpponent(playerId: string): Player[] {
+        const gameName = this.players.get(playerId).gameName;
+        const opponent = this.gamesQueue.get(gameName).pop();
+        const creator = this.gamesQueue.get(gameName).pop();
+        const opponentPlayer = this.players.get(opponent);
+        const creatorPlayer = this.players.get(creator);
+        this.players.delete(opponent);
+        this.players.delete(playerId);
+        return [creatorPlayer, opponentPlayer];
+    }
+
+    deleteOponent(playerId: string): boolean {
+        const opponent = this.gamesQueue.get(this.players.get(playerId).gameName).pop();
+        this.players.delete(opponent);
         return true;
     }
 
-    remove(id: string): boolean {
-        delete this.gamers[id];
-        return true;
-    }
+    // remove(id: string): boolean {
+    //     const player = this.players.get(id);
+    //     this.players.delete(id);
+    //     if (this.gamesQueue.has(player.gameName) && this.gamesQueue.get(player.gameName).length > 0) {
+    //         const size = this.gamesQueue.get(player.gameName).length;
+    //         if (size === 1) {
+    //             this.gamesQueue.get(player.gameName).pop();
+    //         } else if (size === 2) {
+    //             const oponent = this.gamesQueue.get(player.gameName).pop();
+    //             this.players.delete(oponent);
+    //             this.gamesQueue.get(player.gameName).pop();
+    //         }
+    //         return true;
+    //     }
+    //     return false;
+    // }
 }

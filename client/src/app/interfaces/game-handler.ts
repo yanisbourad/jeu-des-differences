c7d544cfbfc751869937dc207fcc836e341530a5
relhe <renellherisson@gmail.com>
2023-03-13 16:39:52 -0400

    add redirect handling

--- client/src/app/interfaces/game-handler.ts
+++ client/src/app/interfaces/game-handler.ts
@@ -3,3 +3,9 @@ export interface Game {
     opponentName: string;
     gameName: string;
 }
+export class GamersInfo {
+    id: number;
+    name: string;
+    opponentName: string;
+    gameName: string;
+}

b8c5944eae5cde757a669df309f89e6b9496d258
relhe <renel.lherisson@polymtl.ca>
2023-03-13 00:50:27 -0400

    html add and logic function

--- client/src/app/components/player-wait-popup/player-wait-popup.component.ts
+++ client/src/app/components/player-wait-popup/player-wait-popup.component.ts
@@ -1,22 +1,60 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AfterContentChecked, Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { Game } from '@app/interfaces/game-handler';
+import { GameCardHandlerService } from '@app/services/game-card-handler-service.service';
 
 @Component({
     selector: 'app-player-wait-popup',
     templateUrl: './player-wait-popup.component.html',
     styleUrls: ['./player-wait-popup.component.scss'],
 })
-export class PlayerWaitPopupComponent implements OnInit {
+export class PlayerWaitPopupComponent implements OnInit, AfterContentChecked {
+    game: Game;
+    // eslint-disable-next-line max-params
     constructor(
-        public dialogRef: MatDialogRef<PlayerWaitPopupComponent>,
+        public dialogReff: MatDialogRef<PlayerWaitPopupComponent>,
+        public gameCardHandlerService: GameCardHandlerService,
         @Inject(MAT_DIALOG_DATA) public data: { name: string; gameName: string; gameType: string },
         private route: Router,
-    ) {}
-    ngOnInit(): void {
-        this.data.name = ' ';
+    ) {
+        this.game = {
+            name: '',
+            opponentName: '',
+            gameName: '',
+        };
     }
-    redirect() {
-        if (this.data.gameType === 'double') this.route.navigate(['/game', { player: this.data.name, gameName: this.data.gameName }]);
+    ngOnInit(): void {
+        this.game.name = this.data.name;
+        this.game.gameName = this.data.gameName;
+        this.game.opponentName = ' ';
+        this.data.name = ' ';
+        this.gameCardHandlerService.join(this.game);
+    }
+
+    ngAfterContentChecked(): void {
+        this.gameCardHandlerService.toggleCreateJoin(this.game.gameName);
+    }
+
+    leaveGame(): void {
+        this.gameCardHandlerService.leave(this.game.gameName);
+        this.dialogReff.close();
+    }
+
+    startGame(): void {
+        this.gameCardHandlerService.startGame(this.game.gameName);
+        this.redirect();
+        this.dialogReff.close();
+    }
+
+    redirect(): void {
+        this.route.navigate([
+            '/game',
+            {
+                player: this.game.name,
+                opponentName: this.game.opponentName,
+                gameName: this.data.gameName,
+            },
+        ]);
     }
 }

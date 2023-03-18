a80e3a4a2b1ae50f8b819ab1e5fc4218537960d6
relhe <renel.lherisson@polymtl.ca>
2023-03-18 18:24:35 -0400

    added method to handle feedback message

---
+++ client/src/app/components/general-feedback/general-feedback.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-general-feedback',
+    templateUrl: './general-feedback.component.html',
+    styleUrls: ['./general-feedback.component.scss'],
+})
+export class GeneralFeedbackComponent implements OnInit {
+    message: string;
+    constructor(public dialogReff: MatDialogRef<GeneralFeedbackComponent>, @Inject(MAT_DIALOG_DATA) public data: { message: string }) {
+        this.message = data.message;
+    }
+
+    ngOnInit(): void {
+        this.message = this.data.message;
+    }
+
+    onClose() {
+        this.message = '';
+        this.dialogReff.close();
+    }
+}

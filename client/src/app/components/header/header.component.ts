7dbf79ce94b1f1890c6bb1af8e9c684e6610e984
LounesSA <77996515+LounesSA@users.noreply.github.com>
2023-03-19 16:21:07 -0400

    LS : redirecting blocked in game page

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TimePopupComponent } from '@app/components/time-popup/time-popup.component';
+import { Router } from '@angular/router';
+// import { GameService } from '@app/services/game.service';
 
 @Component({
     selector: 'app-header',
@@ -11,12 +13,16 @@ export class HeaderComponent {
     @Input() iconUsed: boolean;
     readonly logo: string = 'https://cdn-icons-png.flaticon.com/512/8464/8464334.png';
     readonly title: string = 'VQ';
-    constructor(public dialog: MatDialog) {}
+    constructor(public dialog: MatDialog, private router: Router) {}
     openSettings(): void {
         const dialogRef = this.dialog.open(TimePopupComponent, {
             height: '774px',
             width: '1107px',
         });
         dialogRef.afterClosed();
+    }
+    redirect(): boolean {
+        const newUrl = this.router.url.split(';')[0];
+        return newUrl !== '/game';
     }
 }

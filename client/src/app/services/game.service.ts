2d85d450887358e0c012a66cf7cc67a5aecc1b8b
LounesSA <77996515+LounesSA@users.noreply.github.com>
2023-03-19 19:55:54 -0400

    LS : message for difference and error different

--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -186,16 +186,12 @@ export class GameService {
     }
 
     sendFoundMessage(): void {
+        this.message = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + ' - ' + ' Différence trouvée';
+        if (this.gameType === 'double') {
+            this.message = this.message + ' par ' + this.playerName;
+        }
         const foundMessage = {
-            message:
-                new Date().getHours() +
-                ':' +
-                new Date().getMinutes() +
-                ':' +
-                new Date().getSeconds() +
-                ' - ' +
-                ' Différence trouvée par ' +
-                this.playerName,
+            message: this.message,
             playerName: this.playerName,
             color: '#00FF00',
             pos: '50%',
@@ -215,7 +211,7 @@ export class GameService {
 
     sendErrorMessage(): void {
         this.message = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds() + ' - ' + ' Erreur';
-        if (this.gameType === 'multi') {
+        if (this.gameType === 'double') {
             this.message = this.message + ' par ' + this.playerName;
         }
         const errorMessage = {

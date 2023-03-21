3add828454d904c132e7915460247e4e10f81fca
Hamza Berrada <hamza.berrada@bell.ca>
2023-03-20 20:44:28 -0400

    qq

--- client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
+++ client/src/app/pages/game-creation-page/game-creation-page.component.spec.ts
@@ -5,6 +5,7 @@ import { CanvasNgxComponent } from '@app/components/canvas-ngx/canvas-ngx.compon
 import { DifferencePopupComponent } from '@app/components/difference-popup/difference-popup.component';
 import { BitmapService } from '@app/services/bitmap.service';
 import { CanvasHolderService } from '@app/services/canvas-holder.service';
+import { CommandService } from '@app/services/command.service';
 import { GameCreationPageComponent } from './game-creation-page.component';
 import SpyObj = jasmine.SpyObj;
 
@@ -15,12 +16,16 @@ describe('GameCreationPageComponent', () => {
     let canvasHolderServiceSpy: SpyObj<CanvasHolderService>;
     let renderer2Spy: SpyObj<Renderer2>;
     let matDialogSpy: SpyObj<MatDialog>;
+    let commandServiceSpy: SpyObj<CommandService>;
+    let canvasNgxComponentSpy: SpyObj<CanvasNgxComponent>;
 
     beforeEach(() => {
         canvasHolderServiceSpy = jasmine.createSpyObj('CanvasHolderService', ['originalCanvas', 'modifiedCanvas', 'clearCanvas']);
         bitmapServiceSpy = jasmine.createSpyObj('BitmapService', ['handleFileSelect']);
         renderer2Spy = jasmine.createSpyObj('Renderer2', ['insertBefore']);
         matDialogSpy = jasmine.createSpyObj('MatDialog', ['open', 'afterClosed']);
+        commandServiceSpy = jasmine.createSpyObj('CommandService', ['do']);
+        canvasNgxComponentSpy = jasmine.createSpyObj('CanvasNgxComponent', ['getCanvasDraw', 'getCanvasImage']);
     });
 
     beforeEach(() => {
@@ -32,6 +37,7 @@ describe('GameCreationPageComponent', () => {
                 { provide: CanvasHolderService, useValue: canvasHolderServiceSpy },
                 { provide: Renderer2, useValue: renderer2Spy },
                 { provide: MatDialog, useValue: matDialogSpy },
+                { provide: CommandService, useValue: commandServiceSpy },
             ],
         });
 
@@ -67,4 +73,21 @@ describe('GameCreationPageComponent', () => {
     //     expect(can2).toHaveBeenCalledTimes(1);
     //     expect(bitmapServiceSpy.handleFileSelect).toHaveBeenCalledWith({} as Event);
     // });
+
+    // leftSwapDrawing() {
+    //     const command = new DrawDuplicateDrawing(
+    //         this.modifiedCanvasComponent.getCanvasDraw,
+    //         this.originalCanvasComponent.getCanvasDraw,
+    //         this.modifiedCanvas,
+    //     );
+    //     this.commandService.do(command);
+    // }
+
+    it('should call leftSwapDrawing', () => {
+        component.modifiedCanvasComponent = canvasNgxComponentSpy;
+        component.originalCanvasComponent = canvasNgxComponentSpy;
+        commandServiceSpy.do.and.returnValue();
+        component.leftSwapDrawing();
+        expect(commandServiceSpy.do).toHaveBeenCalledTimes(1);
+    });
 });

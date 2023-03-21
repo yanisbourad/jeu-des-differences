c33869f3941a4d8ca6c3abc2b2078a3e00f31d19
relhe <renellherisson@gmail.com>
2023-03-21 19:37:17 -0400

    completed tests for services

--- server/app/gateways/game-card-handler/game-card-handler.service.spec.ts
+++ server/app/gateways/game-card-handler/game-card-handler.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FAKE_QUEUE, OVER_CROWDED } from './entities/constants';
+import { Player } from './entities/player.entity';
 import { GameCardHandlerService } from './game-card-handler.service';
 
 describe('GameCardHandlerService', () => {
@@ -111,13 +112,12 @@ describe('GameCardHandlerService', () => {
 
     it('should return true if player is waiting', () => {
         service.gamesQueue.set('uno', ['rac']);
-        expect(
-            service.isPlayerWaiting({
-                id: 'rac',
-                name: 'John Do',
-                gameName: 'uno',
-            }),
-        ).toBeTruthy();
+        const player: Player = {
+            id: 'rac',
+            name: 'John Do',
+            gameName: 'uno',
+        };
+        expect(service.isPlayerWaiting(player)).toBeTruthy();
     });
 
     it('should be true as a player is waiting', () => {

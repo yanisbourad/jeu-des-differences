d700fea9250915f3a46dff29f85f81e7209abffe
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-03-21 20:21:00 -0400

    GMJ-bad constants removed

--- client/src/app/configuration/const-game.ts
+++ client/src/app/configuration/const-game.ts
@@ -1,12 +1,1 @@
 export const WAITING_TIME = 250;
-export const CHAT_BOX_DEMO = 26;
-export const NUMBER_OF_HINTS = 3;
-export const HINTS_PENALTY = 5;
-export const ZERO = 0;
-export const ONE = 1;
-export const TWO = 2;
-export const THREE = 3;
-export const FOUR = 4;
-export const FIVE = 5;
-export const EIGHT = 8;
-export const MINUS_ONE = -1;

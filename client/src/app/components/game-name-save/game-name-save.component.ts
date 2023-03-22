1ee117c5450bc9cd6c3aafb486b3951015838755
Hamza Berrada <hamza.berrada@bell.ca>
2023-03-21 22:02:49 -0400

    HB: Handle name Game incorrect

--- client/src/app/components/game-name-save/game-name-save.component.ts
+++ client/src/app/components/game-name-save/game-name-save.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { GeneralFeedbackComponent } from '@app/components/general-feedback/general-feedback.component';
 import { GameDatabaseService } from '@app/services/game-database.service';
 import { ImageDiffService } from '@app/services/image-diff.service';
 
@@ -15,12 +16,14 @@ export class GameNameSaveComponent {
     showFeedback: string;
     lowerLengthNameLimit: number;
     upperLengthNameLimit: number;
+
     // eslint-disable-next-line max-params
     constructor(
         public dialogRef: MatDialogRef<GameNameSaveComponent>,
         private readonly gameDataBase: GameDatabaseService,
         private differenceService: ImageDiffService,
         private router: Router,
+        public dialog: MatDialog,
     ) {
         this.gameName = '';
         this.showFeedback = '';
@@ -32,23 +35,38 @@ export class GameNameSaveComponent {
     validateGameName(name: string): boolean {
         const len = name.trim() === '';
         if (!len && name.length > this.lowerLengthNameLimit && name.length < this.upperLengthNameLimit) return true;
+        this.launchFeedback('Le nom du jeu doit être compris entre 3 et 10 caractères.');
         return false;
     }
 
     getGameData() {
         if (this.validateGameName(this.gameName.toLocaleLowerCase())) {
-            if (!this.gameDataBase.saveGame(this.gameName)) {
-                alert('/"Nom indisponible: Entrer un autre nom SVP!/"');
-            }
-            this.level = '';
-            this.showFeedback = '';
-            this.dialogRef.close();
-            this.router.navigate(['/config']);
+            const isSaved = this.gameDataBase.saveGame(this.gameName);
+            isSaved.subscribe((response) => {
+                isSaved.unsubscribe();
+                if (response) {
+                    this.level = '';
+                    this.showFeedback = '';
+                    this.dialogRef.close();
+                    this.router.navigate(['/config']);
+                    this.launchFeedback('Votre Jeu a été sauvegardée avec succès!');
+                } else {
+                    this.showFeedback = 'The game was not saved';
+                    this.launchFeedback('Ce nom est déjà utilisé, Veille choisir un autre.');
+                }
+            });
         }
         this.gameName = '';
     }
 
     closeOnAbort() {
         this.dialogRef.close();
+    }
+
+    launchFeedback(showedMessage: string): void {
+        this.dialog.open(GeneralFeedbackComponent, {
+            data: { message: showedMessage },
+            disableClose: true,
+        });
     }
 }

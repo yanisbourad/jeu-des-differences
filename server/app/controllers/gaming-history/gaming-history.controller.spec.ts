084403a43dd3f8a1410ac6f2a8f70ce51e4888e4
relhe <renel.lherisson@polymtl.ca>
2023-04-01 18:27:16 -0400

    added test gor get method

--- server/app/controllers/gaming-history/gaming-history.controller.spec.ts
+++ server/app/controllers/gaming-history/gaming-history.controller.spec.ts
@@ -1,6 +1,8 @@
 import { GamingHistory } from '@app/model/database/gaming-history';
 import { GameRecordService } from '@app/services/game-record/game-record.service';
+import { HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Response } from 'express';
 import { SinonStubbedInstance, createStubInstance } from 'sinon';
 import { GamingHistoryController } from './gaming-history.controller';
 
@@ -32,6 +34,40 @@ describe('GamingHistoryController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    // test for allGames()
+    it('should return all gaming History', async () => {
+        jest.spyOn(gameRecordService, 'getAllGamingHistory').mockImplementation(async () => {
+            return Promise.resolve([stubGamingHistory]);
+        });
+        const res = {} as unknown as Response;
+        res.json = (body) => {
+            expect(body).toStrictEqual([stubGamingHistory]);
+            return res;
+        };
+        res.status = (code) => {
+            expect(code).toBe(HttpStatus.OK);
+            return res;
+        };
+        await controller.allGames(res);
+        expect(gameRecordService.getAllGamingHistory).toHaveBeenCalled();
+    });
+
+    it('should return NOT_FOUND http status when request fails', async () => {
+        jest.spyOn(gameRecordService, 'getAllGamingHistory').mockRejectedValueOnce(new Error('test error'));
+        const res = {} as unknown as Response;
+        res.status = (code) => {
+            expect(code).toBe(HttpStatus.NOT_FOUND);
+            return res;
+        };
+        res.send = (message) => {
+            expect(message).toBe('test error');
+            return res;
+        };
+        res.send = () => res;
+        await controller.allGames(res);
+        expect(gameRecordService.getAllGamingHistory).toHaveBeenCalled();
     });
 
 

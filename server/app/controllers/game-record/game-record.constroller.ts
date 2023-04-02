0c3678033446bc0205a7e9ec27081b8570f5a875
relhe <renel.lherisson@polymtl.ca>
2023-04-02 09:52:31 -0400

    added delete for CRUD in controller

--- server/app/controllers/game-record/game-record.constroller.ts
+++ server/app/controllers/game-record/game-record.constroller.ts
@@ -1,31 +1,14 @@
 import { GameRecord } from '@app/model/database/game-record';
 import { CreateGameRecordDto } from '@app/model/dto/game-record/create-game-record.dto';
 import { GameRecordService } from '@app/services/game-record/game-record.service';
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
-import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, HttpStatus, Post, Res } from '@nestjs/common';
+import { ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
 @ApiTags('GamesRecord')
 @Controller('gameRecord')
 export class GameRecordController {
     constructor(private readonly gamesRecordService: GameRecordService) {}
-
-    // @ApiOkResponse({
-    //     description: 'Returns all GameRecords',
-    //     isArray: true,
-    // })
-    // @ApiNotFoundResponse({
-    //     description: 'Return NOT_FOUND http status when request fails',
-    // })
-    // @Get('/')
-    // async allGames(@Res() response: Response) {
-    //     try {
-    //         const allGames = await this.gamesRecordService.getAllGameRecord();
-    //         response.status(HttpStatus.OK).json(allGames);
-    //     } catch (error) {
-    //         response.status(HttpStatus.NOT_FOUND).send(error.message);
-    //     }
-    // }
 
     @ApiOkResponse({
         description: 'Create gameRecord',
@@ -43,36 +26,20 @@ export class GameRecordController {
             response.status(HttpStatus.NOT_FOUND).send(error.message);
         }
     }
-    // @ApiOkResponse({
-    //     description: 'Create gameHistory',
-    //     type: GamingHistory,
-    // })
-    // @ApiNotFoundResponse({
-    //     description: 'Return NOT_FOUND http status when request fails',
-    // })
-    // @Post('/create')
-    // async createGamingHistory(@Body() gameRecord: CreateGamingHistoryDto, @Res() response: Response) {
-    //     try {
-    //         const res = await this.gamesRecordService.addGamingHistory(gameRecord);
-    //         response.status(HttpStatus.OK).json(res);
-    //     } catch (error) {
-    //         response.status(HttpStatus.NOT_FOUND).send(error.message);
-    //     }
-    // }
 
-    // @ApiOkResponse({
-    //     description: 'Delete gameRecords',
-    // })
-    // @ApiNotFoundResponse({
-    //     description: 'Return NOT_FOUND http status when request fails',
-    // })
-    // @Delete('/')
-    // async deleteGame(@Res() response: Response) {
-    //     try {
-    //         await this.gamesRecordService.deleteGameRecords();
-    //         response.status(HttpStatus.OK).json('Game deleted successfully');
-    //     } catch (error) {
-    //         response.status(HttpStatus.BAD_REQUEST).send(error.message);
-    //     }
-    // }
+    @ApiOkResponse({
+        description: 'Delete gameRecords',
+    })
+    @ApiNoContentResponse({
+        description: 'Return NO_CONTENT http status when request fails',
+    })
+    @Delete('/')
+    async deleteAllGameRecords(@Res() response: Response) {
+        try {
+            await this.gamesRecordService.deleteGameRecords();
+            response.status(HttpStatus.OK).json('Game deleted successfully');
+        } catch (error) {
+            response.status(HttpStatus.NO_CONTENT).send(error.message);
+        }
+    }
 }

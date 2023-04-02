261d79553fb50b5f188be30f7f131a87071c7c77
relhe <renel.lherisson@polymtl.ca>
2023-04-02 13:47:11 -0400

    added delete records method for on gameCards and get method for controller

--- server/app/controllers/game-record/game-record.constroller.ts
+++ server/app/controllers/game-record/game-record.constroller.ts
@@ -1,7 +1,7 @@
 import { GameRecord } from '@app/model/database/game-record';
 import { CreateGameRecordDto } from '@app/model/dto/game-record/create-game-record.dto';
 import { GameRecordService } from '@app/services/game-record/game-record.service';
-import { Body, Controller, Delete, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
@@ -9,6 +9,24 @@ import { Response } from 'express';
 @Controller('gameRecord')
 export class GameRecordController {
     constructor(private readonly gamesRecordService: GameRecordService) {}
+
+    @ApiOkResponse({
+        description: 'Returns all GamingHistory',
+        isArray: true,
+    })
+    @ApiNotFoundResponse({
+        description: 'Return NOT_FOUND http status when request fails',
+    })
+    @Get('/')
+    async allGames(@Res() response: Response) {
+        try {
+            const allGames = await this.gamesRecordService.getAllGameRecord();
+            response.status(HttpStatus.OK).json(allGames);
+        } catch (error) {
+            response.status(HttpStatus.NOT_FOUND).send(error.message);
+        }
+    }
+
 
     @ApiOkResponse({
         description: 'Create gameRecord',
@@ -28,7 +46,7 @@ export class GameRecordController {
     }
 
     @ApiOkResponse({
-        description: 'Delete gameRecords',
+        description: 'Delete gameRecords for all games',
     })
     @ApiNoContentResponse({
         description: 'Return NO_CONTENT http status when request fails',
@@ -37,7 +55,22 @@ export class GameRecordController {
     async deleteAllGameRecords(@Res() response: Response) {
         try {
             await this.gamesRecordService.deleteGameRecords();
-            response.status(HttpStatus.OK).json('Game deleted successfully');
+            response.status(HttpStatus.OK).json('Game records were deleted successfully');
+        } catch (error) {
+            response.status(HttpStatus.NO_CONTENT).send(error.message);
+        }
+    }
+    @ApiOkResponse({
+        description: 'Delete gameRecords for one game',
+    })
+    @ApiNoContentResponse({
+        description: 'Return NO_CONTENT http status when request fails',
+    })
+    @Delete('/:gameName')
+    async deleteSpecificGameRecords(@Param('gameName') gameName: string, @Res() response: Response) {
+        try {
+            await this.gamesRecordService.deleteGameRecordsForOneGame(gameName);
+            response.status(HttpStatus.OK).json('Game record was deleted successfully');
         } catch (error) {
             response.status(HttpStatus.NO_CONTENT).send(error.message);
         }

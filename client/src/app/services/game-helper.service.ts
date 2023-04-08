b6caaa1a76d7ad407d79066639ab8dbd08e630d4
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2023-04-07 23:51:03 -0400

    GMJ - move some functions to gameHelper, something is strange with the detection difference, sometimes work or not

--- client/src/app/services/game-helper.service.ts
+++ client/src/app/services/game-helper.service.ts
@@ -1,9 +1,209 @@
 import { Injectable } from '@angular/core';
+import * as constantsTime from '@app/configuration/const-time';
+import { GameRecord } from '@common/game';
+import { SocketClientService } from './socket-client.service';
+import { ImagePath } from '@app/interfaces/hint-diff-path';
+import { MessageDialogComponent } from '@app/components/message-dialog/message-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Message } from '@app/interfaces/message';
 
 @Injectable({
     providedIn: 'root',
 })
 // to contain various helpful functions
 export class GameHelperService {
-    constructor() {}
+    path: ImagePath;
+    rankingSoloCopy: GameRecord[];
+    rankingMultiCopy: GameRecord[];
+    playerName: string;
+    gameName: string;
+    gameType: string;
+    message: string;
+    constructor(private socket: SocketClientService, private dialog: MatDialog) {
+        this.path = {
+            differenceNotFound: './assets/img/difference-not-found.png',
+            differenceFound: './assets/img/difference-found.png',
+        };
+    }
+
+    getGameTime(gameTime: number): string {
+        const minutes = Math.floor(gameTime / constantsTime.MIN_TO_SEC);
+        const seconds = gameTime - minutes * constantsTime.MIN_TO_SEC;
+        return `${minutes}:${seconds < constantsTime.UNIT ? '0' : ''}${seconds}`;
+    }
+
+    displayGameEnded(msg: string, type: string, time?: string) {
+        this.dialog.open(MessageDialogComponent, {
+            data: { message: msg, type, formatTime: time },
+            disableClose: true,
+            minWidth: 'fit-content',
+            minHeight: 'fit-content',
+            panelClass: 'custom-dialog-container',
+        });
+    }
+
+    displayGiveUp(msg: string, type: string): void {
+        this.dialog.open(MessageDialogComponent, {
+            data: { message: msg, type },
+            minWidth: 'fit-content',
+            minHeight: 'fit-content',
+            panelClass: 'custom-dialog-container',
+        });
+    }
+
+    sendFoundMessage(): Message {
+        this.message = new Date().toLocaleTimeString() + ' - ' + ' Différence trouvée';
+        if (this.gameType === 'double') {
+            this.message = this.message + ' par ' + this.playerName;
+        }
+        const foundMessage = {
+            message: this.message,
+            playerName: this.playerName,
+            color: '#00FF00',
+            pos: '50%',
+            gameId: this.socket.getRoomName(),
+            event: true,
+        };
+        this.socket.sendMessage(foundMessage);
+        return {
+            message: foundMessage.message,
+            userName: foundMessage.playerName,
+            mine: true,
+            color: foundMessage.color,
+            pos: foundMessage.pos,
+            event: foundMessage.event,
+        };
+    }
+
+    sendErrorMessage(): Message {
+        this.message = new Date().toLocaleTimeString() + ' - ' + ' Erreur';
+        if (this.gameType === 'double') {
+            this.message = this.message + ' par ' + this.playerName;
+        }
+        const errorMessage = {
+            message: this.message,
+            playerName: this.playerName,
+            color: '#FF0000',
+            pos: '50%',
+            gameId: this.socket.getRoomName(),
+            event: true,
+        };
+        this.socket.sendMessage(errorMessage);
+
+        return {
+            message: errorMessage.message,
+            userName: errorMessage.playerName,
+            mine: true,
+            color: errorMessage.color,
+            pos: errorMessage.pos,
+            event: errorMessage.event,
+        };
+    }
+
+    sendWinnerMessage(position: number): void {
+        let msg = '';
+        switch (position) {
+            case 1:
+                msg = ' obtient la première position dans les meilleurs temps du jeu ';
+                break;
+            case 2:
+                msg = ' obtient la deuxième position dans les meilleurs temps du jeu ';
+                break;
+            case 3:
+                msg = ' obtient la troisième dans les meilleurs temps du jeu ';
+                break;
+            default:
+                break;
+        }
+        this.message = new Date().toLocaleTimeString() + ' - ' + this.playerName + msg + this.gameName + ' en ' + this.gameType;
+        const winnerMessage = {
+            message: this.message,
+            playerName: 'meilleur temps',
+            color: '#FF0000',
+            pos: '50%',
+            gameId: this.socket.getRoomName(),
+            event: true,
+        };
+        this.socket.sendMessage(winnerMessage);
+        this.socket.messageList.push({
+            message: winnerMessage.message,
+            userName: winnerMessage.playerName,
+            mine: true,
+            color: winnerMessage.color,
+            pos: winnerMessage.pos,
+            event: winnerMessage.event,
+        });
+    }
+
+    globalMessageMulti(gameTime: number) {
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[0].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingMultiCopy[0].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingMultiCopy[0].time.slice(2), 10)) {
+                    this.sendWinnerMessage(1);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(1);
+                return;
+            }
+        }
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[1].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingMultiCopy[1].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingMultiCopy[1].time.slice(2), 10)) {
+                    this.sendWinnerMessage(2);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(2);
+                return;
+            }
+        }
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[2].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingMultiCopy[2].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingMultiCopy[2].time.slice(2), 10)) {
+                    this.sendWinnerMessage(3);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(3);
+                return;
+            }
+        }
+    }
+
+    globalMessageSolo(gameTime: number) {
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[0].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingSoloCopy[0].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingSoloCopy[0].time.slice(2), 10)) {
+                    this.sendWinnerMessage(1);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(1);
+                return;
+            }
+        }
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[1].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingSoloCopy[1].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingSoloCopy[1].time.slice(2), 10)) {
+                    this.sendWinnerMessage(2);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(2);
+                return;
+            }
+        }
+        if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[2].time.slice(0, 2), 10)) {
+            if (parseInt(this.getGameTime(gameTime).slice(0, 2), 10) === parseInt(this.rankingSoloCopy[2].time.slice(0, 2), 10)) {
+                if (parseInt(this.getGameTime(gameTime).slice(2), 10) <= parseInt(this.rankingSoloCopy[2].time.slice(2), 10)) {
+                    this.sendWinnerMessage(3);
+                    return;
+                }
+            } else {
+                this.sendWinnerMessage(3);
+                return;
+            }
+        }
+    }
 }
--- client/src/app/services/game.service.ts
+++ client/src/app/services/game.service.ts
@@ -1,13 +1,10 @@
 /* eslint-disable max-lines */
 import { HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { MessageDialogComponent } from '@app/components/message-dialog/message-dialog.component';
 import { MouseButton } from '@app/components/play-area/play-area.component';
 import * as constants from '@app/configuration/const-canvas';
 import * as constantsTime from '@app/configuration/const-time';
 import { GameInformation } from '@app/interfaces/game-information';
-import { ImagePath } from '@app/interfaces/hint-diff-path';
 import { Message } from '@app/interfaces/message';
 import { Vec2 } from '@app/interfaces/vec2';
 import { GameDatabaseService } from '@app/services/game-database.service';
@@ -15,12 +12,12 @@ import { SocketClientService } from '@app/services/socket-client.service';
 import { Game, GameRecord, GamingHistory } from '@common/game';
 import { Observable } from 'rxjs';
 import { GameCardHandlerService } from './game-card-handler-service.service';
+import { GameHelperService } from './game-helper.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameService {
-    path: ImagePath; // can be moved
     rankingSoloCopy: GameRecord[];
     rankingMultiCopy: GameRecord[];
     game: Game;
@@ -38,7 +35,6 @@ export class GameService {
     gameName: string;
     mode: string;
     diff: Set<number>;
-    message: string;
     mousePosition: Vec2;
     errorPenalty: boolean;
     isWinner: boolean;
@@ -48,14 +44,10 @@ export class GameService {
     // eslint-disable-next-line max-params
     constructor(
         private readonly gameCardHandlerService: GameCardHandlerService,
-        public dialog: MatDialog,
         private gameDataBase: GameDatabaseService,
         private socket: SocketClientService,
+        private gameHelper: GameHelperService,
     ) {
-        this.path = {
-            differenceNotFound: './assets/img/difference-not-found.png',
-            differenceFound: './assets/img/difference-found.png',
-        };
         this.gameInformation = {
             gameTitle: '',
             gameMode: '',
@@ -75,7 +67,6 @@ export class GameService {
     }
 
     defineVariables(): void {
-        // console.log(this.game);
         this.gameInformation = {
             gameTitle: this.game.gameName,
             gameMode: this.gameType,
@@ -87,6 +78,11 @@ export class GameService {
         this.differencesArray = new Array(this.totalDifferences);
         this.opponentDifferencesArray = new Array(this.totalDifferences);
         this.playersName = [this.playerName, this.opponentName];
+        this.gameHelper.gameName = this.gameName;
+        this.gameHelper.gameType = this.gameType;
+        this.gameHelper.playerName = this.playerName;
+        this.gameHelper.rankingSoloCopy = this.rankingSoloCopy;
+        this.gameHelper.rankingMultiCopy = this.rankingMultiCopy;
     }
 
     getSetDifference(differencesStr: string[]): Set<number>[] {
@@ -113,8 +109,8 @@ export class GameService {
         // this.socket.gameTime = 0;
         this.totalDifferences = this.gameInformation.nDifferences;
         this.differencesArray = new Array(this.totalDifferences);
-        this.displayIcons();
         this.opponentDifferencesArray = new Array(this.totalDifferences);
+        this.displayIcons();
         this.socket.messageList = [];
         // this.playersName = [this.playerName, this.opponentName];
     }
@@ -122,13 +118,13 @@ export class GameService {
     displayIcons(): void {
         if (this.gameType === 'solo') {
             for (let i = 0; i < this.totalDifferences; i++) {
-                this.differencesArray[i] = this.path.differenceNotFound;
+                this.differencesArray[i] = this.gameHelper.path.differenceNotFound;
             }
         }
         if (this.gameType === 'double' && !this.mode) {
             for (let i = 0; i < this.totalDifferences; i++) {
-                this.differencesArray[i] = this.path.differenceNotFound;
-                this.opponentDifferencesArray[i] = this.path.differenceNotFound;
+                this.differencesArray[i] = this.gameHelper.path.differenceNotFound;
+                this.opponentDifferencesArray[i] = this.gameHelper.path.differenceNotFound;
             }
         }
     }
@@ -137,19 +133,12 @@ export class GameService {
         const diffFound = this.socket.nbrDifference - this.socket.diffLeft;
         for (let i = 0; i < diffFound; i++) {
             this.differencesArray.pop();
-            this.differencesArray.unshift(this.path.differenceFound);
+            this.differencesArray.unshift(this.gameHelper.path.differenceFound);
         }
     }
 
     displayGameEnded(msg: string, type: string, time?: string) {
-        // can be moved to a service
-        this.dialog.open(MessageDialogComponent, {
-            data: { message: msg, type, formatTime: time },
-            disableClose: true,
-            minWidth: 'fit-content',
-            minHeight: 'fit-content',
-            panelClass: 'custom-dialog-container',
-        });
+        this.gameHelper.displayGameEnded(msg, type, time);
     }
 
     setStartDate(date: string): void {
@@ -171,7 +160,7 @@ export class GameService {
     reduceNbrDifferences(): void {
         this.nDifferencesFound++;
         this.differencesArray.pop();
-        this.differencesArray.unshift(this.path.differenceFound);
+        this.differencesArray.unshift(this.gameHelper.path.differenceFound);
     }
 
     handleDifferenceFound(): void {
@@ -199,13 +188,14 @@ export class GameService {
             this.endGame();
         }
     }
+
     totalDifferenceReached(): boolean {
         return this.nDifferencesFound === this.totalDifferences;
     }
 
     handlePlayerDifference() {
         this.opponentDifferencesArray.pop();
-        this.opponentDifferencesArray.unshift(this.path.differenceFound);
+        this.opponentDifferencesArray.unshift(this.gameHelper.path.differenceFound);
     }
 
     multiGameEnd(): boolean {
@@ -216,184 +206,24 @@ export class GameService {
     }
 
     sendFoundMessage(): Message {
-        this.message = new Date().toLocaleTimeString() + ' - ' + ' Différence trouvée';
-        if (this.gameType === 'double') {
-            this.message = this.message + ' par ' + this.playerName;
-        }
-        const foundMessage = {
-            message: this.message,
-            playerName: this.playerName,
-            color: '#00FF00',
-            pos: '50%',
-            gameId: this.socket.getRoomName(),
-            event: true,
-        };
-        this.socket.sendMessage(foundMessage);
-        return {
-            message: foundMessage.message,
-            userName: foundMessage.playerName,
-            mine: true,
-            color: foundMessage.color,
-            pos: foundMessage.pos,
-            event: foundMessage.event,
-        };
+        return this.gameHelper.sendFoundMessage();
     }
 
     sendErrorMessage(): Message {
-        this.message = new Date().toLocaleTimeString() + ' - ' + ' Erreur';
-        if (this.gameType === 'double') {
-            this.message = this.message + ' par ' + this.playerName;
-        }
-        const errorMessage = {
-            message: this.message,
-            playerName: this.playerName,
-            color: '#FF0000',
-            pos: '50%',
-            gameId: this.socket.getRoomName(),
-            event: true,
-        };
-        this.socket.sendMessage(errorMessage);
-
-        return {
-            message: errorMessage.message,
-            userName: errorMessage.playerName,
-            mine: true,
-            color: errorMessage.color,
-            pos: errorMessage.pos,
-            event: errorMessage.event,
-        };
-    }
-    sendWinnerMessage(position: number): void {
-        if (position === 1) {
-            this.message =
-                new Date().toLocaleTimeString() +
-                ' - ' +
-                this.playerName +
-                ' obtient la première position dans les meilleurs temps du jeu ' +
-                this.gameName +
-                ' en ' +
-                this.gameType;
-        }
-        if (position === 2) {
-            this.message =
-                new Date().toLocaleTimeString() +
-                ' - ' +
-                this.playerName +
-                ' obtient la deuxième position dans les meilleurs temps du jeu ' +
-                this.gameName +
-                ' en ' +
-                this.gameType;
-        }
-        if (position === 3) {
-            this.message =
-                new Date().toLocaleTimeString() +
-                ' - ' +
-                this.playerName +
-                ' obtient la troisième dans les meilleurs temps du jeu ' +
-                this.gameName +
-                ' en ' +
-                this.gameType;
-        }
-        const winnerMessage = {
-            message: this.message,
-            playerName: 'meilleur temps',
-            color: '#FF0000',
-            pos: '50%',
-            gameId: this.socket.getRoomName(),
-            event: true,
-        };
-        this.socket.sendMessage(winnerMessage);
-        this.socket.messageList.push({
-            message: winnerMessage.message,
-            userName: winnerMessage.playerName,
-            mine: true,
-            color: winnerMessage.color,
-            pos: winnerMessage.pos,
-            event: winnerMessage.event,
-        });
-    }
-    globalMessageMulti() {
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[0].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingMultiCopy[0].time.slice(0, 2), 10)) {
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingMultiCopy[0].time.slice(2), 10)) {
-                    this.sendWinnerMessage(1);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(1);
-                return;
-            }
-        }
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[1].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingMultiCopy[1].time.slice(0, 2), 10)) {
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingMultiCopy[1].time.slice(2), 10)) {
-                    this.sendWinnerMessage(2);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(2);
-                return;
-            }
-        }
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingMultiCopy[2].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingMultiCopy[2].time.slice(0, 2), 10)) {
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingMultiCopy[2].time.slice(2), 10)) {
-                    this.sendWinnerMessage(3);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(3);
-                return;
-            }
-        }
-    }
-    globalMessageSolo() {
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[0].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingSoloCopy[0].time.slice(0, 2), 10)) {
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingSoloCopy[0].time.slice(2), 10)) {
-                    this.sendWinnerMessage(1);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(1);
-                return;
-            }
-        }
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[1].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingSoloCopy[1].time.slice(0, 2), 10)){
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingSoloCopy[1].time.slice(2), 10)) {
-                    this.sendWinnerMessage(2);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(2);
-                return;
-            }
-        }
-        if (parseInt(this.getGameTime().slice(0, 2), 10) <= parseInt(this.rankingSoloCopy[2].time.slice(0, 2), 10)) {
-            if (parseInt(this.getGameTime().slice(0, 2), 10) === parseInt(this.rankingSoloCopy[2].time.slice(0, 2), 10)) {
-                if (parseInt(this.getGameTime().slice(2), 10) <= parseInt(this.rankingSoloCopy[2].time.slice(2), 10)) {
-                    this.sendWinnerMessage(3);
-                    return;
-                }
-            } else {
-                this.sendWinnerMessage(3);
-                return;
-            }
-        }
+        return this.gameHelper.sendErrorMessage();
     }
 
     endGame(): void {
         this.socket.stopTimer(this.socket.getRoomName(), this.playerName);
-        this.socket.gameEnded(this.socket.getRoomName());
         this.gameTime = this.socket.getRoomTime(this.socket.getRoomName());
         this.saveGameRecord();
         if (this.gameType === 'solo') {
-            this.globalMessageSolo();
+            this.gameHelper.globalMessageSolo(this.gameTime);
         } else {
-            this.globalMessageMulti();
+            this.gameHelper.globalMessageMulti(this.gameTime);
         }
-        this.displayGameEnded('Félicitation, vous avez terminée la partie', 'finished', this.getGameTime());
+        this.displayGameEnded('Félicitation, vous avez terminée la partie', 'finished', this.gameHelper.getGameTime(this.gameTime));
+        this.socket.gameEnded(this.socket.getRoomName());
         this.reinitializeGame();
     }
 
@@ -403,12 +233,12 @@ export class GameService {
             typeGame: this.gameType === 'double' ? 'multi' : 'solo',
             playerName: this.playerName,
             dateStart: this.startDate,
-            time: this.getGameTime(),
+            time: this.gameHelper.getGameTime(this.gameTime),
         };
         const gamingHistory: GamingHistory = {
             gameName: this.gameInformation.gameTitle,
             dateStart: this.startDate,
-            time: this.getGameTime(),
+            time: this.gameHelper.getGameTime(this.gameTime),
             gameType: this.gameType === 'double' ? 'multi' : 'solo',
             playerName: this.playerName,
             opponentName: this.gameType === 'double' ? this.opponentName : '999999999999999',
@@ -418,13 +248,6 @@ export class GameService {
         if (this.gameType === 'solo' || this.isWinner) {
             this.gameDataBase.createGamingHistory(gamingHistory).subscribe();
         }
-    }
-
-    getGameTime(): string {
-        // can be moved
-        const minutes = Math.floor(this.gameTime / constantsTime.MIN_TO_SEC);
-        const seconds = this.gameTime - minutes * constantsTime.MIN_TO_SEC;
-        return `${minutes}:${seconds < constantsTime.UNIT ? '0' : ''}${seconds}`;
     }
 
     deleteGame(gameName: string): Observable<HttpResponse<string>> {
@@ -448,23 +271,13 @@ export class GameService {
         }, constantsTime.BLINKING_TIME);
     }
 
-    displayGiveUp(msg: string, type: string): void {
-        // can be moved
-        this.dialog.open(MessageDialogComponent, {
-            data: { message: msg, type },
-            minWidth: 'fit-content',
-            minHeight: 'fit-content',
-            panelClass: 'custom-dialog-container',
-        });
-    }
-
     giveUp(): void {
-        // can be moved
         this.hasAbandonedGame = true;
         this.saveGameRecord();
         this.reinitializeGame();
-        this.displayGiveUp('Êtes-vous sûr de vouloir abandonner la partie? Cette action est irréversible.', 'giveUp');
+        this.gameHelper.displayGiveUp('Êtes-vous sûr de vouloir abandonner la partie? Cette action est irréversible.', 'giveUp');
     }
+
     deleteOneGameRecords(gameName: string): void {
         this.gameDataBase.deleteOneGameRecords(gameName).subscribe();
     }

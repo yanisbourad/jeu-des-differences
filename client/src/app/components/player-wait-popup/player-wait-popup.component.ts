5fdc96fe582ef3c293b9d3c1c924448341580865
relhe <renel.lherisson@polymtl.ca>
2023-04-08 20:14:44 -0400

    updated player-wait component -- added new attribute to player obj

--- client/src/app/components/player-wait-popup/player-wait-popup.component.ts
+++ client/src/app/components/player-wait-popup/player-wait-popup.component.ts
@@ -31,6 +31,7 @@ export class PlayerWaitPopupComponent implements OnInit, AfterContentChecked {
             name: '',
             opponentName: '',
             gameName: '',
+            gameType: '',
         };
         this.isReady = false;
         this.isUpdated = false;
@@ -45,6 +46,7 @@ export class PlayerWaitPopupComponent implements OnInit, AfterContentChecked {
         this.game.name = this.data.name;
         this.game.gameName = this.data.gameName;
         this.game.opponentName = "Attente d'un adversaire";
+        this.game.gameType = this.data.gameType === 'double' ? 'Double' : 'limit';
         this.data.name = ' ';
         this.gameCardHandlerService.join(this.game);
         this.gameCardHandlerService.toggleCreateJoin(this.game.gameName);

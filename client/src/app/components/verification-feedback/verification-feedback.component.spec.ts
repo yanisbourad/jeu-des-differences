f471b42666879f573c74d70a2d480969834b65af
relhe <renellherisson@gmail.com>
2023-04-16 14:13:42 -0400

    fix lint for test verif feed

--- client/src/app/components/verification-feedback/verification-feedback.component.spec.ts
+++ client/src/app/components/verification-feedback/verification-feedback.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VerificationFeedbackComponent } from './verification-feedback.component';
 
 describe('VerificationFeedbackComponent', () => {
-  let component: VerificationFeedbackComponent;
-  let fixture: ComponentFixture<VerificationFeedbackComponent>;
+    let component: VerificationFeedbackComponent;
+    let fixture: ComponentFixture<VerificationFeedbackComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VerificationFeedbackComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [VerificationFeedbackComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(VerificationFeedbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(VerificationFeedbackComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });

756f6ea574a369f245eeab5269eab978633b2aac
Daniel Giao <daniel.giao@icloud.com>
2023-04-18 15:03:25 -0400

    hints-display test DONE

--- client/src/app/services/hints/hints-display.service.spec.ts
+++ client/src/app/services/hints/hints-display.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HintsDisplayService } from './hints-display.service';
+import { SocketClientService } from '@app/services/socket/socket-client.service';
+import SpyObj = jasmine.SpyObj;
 
-describe('HintsDisplayService', () => {
-  let service: HintsDisplayService;
+fdescribe('HintsDisplayService', () => {
+    let hintsDisplayService: HintsDisplayService;
+    let socketClientServiceSpy: SpyObj<SocketClientService>;
+    let foundMessage: { message: string; playerName: string; color: string; pos: string; gameId: string; event: boolean };
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HintsDisplayService);
-  });
+    beforeEach(() => {
+        socketClientServiceSpy = jasmine.createSpyObj('SocketClientService', ['sendMessage', 'getRoomName', 'modifyTime']);
+        TestBed.configureTestingModule({
+            providers: [{ provide: SocketClientService, useValue: socketClientServiceSpy }],
+        });
+        hintsDisplayService = TestBed.inject(HintsDisplayService);
+        foundMessage = { message: hintsDisplayService.message, ...hintsDisplayService.subMessage };
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(hintsDisplayService).toBeTruthy();
+    });
+
+    it('setIcons should set hintsArray', () => {
+        hintsDisplayService.setIcons();
+        for (let i = 0; i < hintsDisplayService.totalHints; i++) {
+            expect(hintsDisplayService.hintsArray[i]).toEqual(hintsDisplayService.path.hintsNotUsed);
+        }
+        expect(hintsDisplayService.hintsArray.length).toBe(3);
+    });
+
+    it('updateIcons should  and switch path in differenceArray ', () => {
+        hintsDisplayService.updateIcons();
+        expect(hintsDisplayService.hintsArray[2]).toEqual(hintsDisplayService.path.hintsUsed);
+        expect(hintsDisplayService.hintsArray[1]).toEqual(hintsDisplayService.path.hintsNotUsed);
+        expect(hintsDisplayService.hintsArray[0]).toEqual(hintsDisplayService.path.hintsNotUsed);
+    });
+
+    it('sendFoundMessage message should be "Indice utilisé"', () => {
+        hintsDisplayService.sendHintMessage();
+        expect(hintsDisplayService.message).toBe(new Date().toLocaleTimeString() + ' - ' + ' Indice utilisé');
+    });
+
+    it('sendHintMessage should send message via socket', () => {
+        foundMessage.message = new Date().toLocaleTimeString() + ' - ' + ' Indice utilisé';
+        hintsDisplayService.sendHintMessage();
+        expect(socketClientServiceSpy.sendMessage).toHaveBeenCalledWith(foundMessage);
+    });
+
+    it('sendHintMessage should return message with mine flag', () => {
+        foundMessage.message = new Date().toLocaleTimeString() + ' - ' + ' Indice utilisé';
+        const testResult = hintsDisplayService.sendHintMessage();
+        expect(testResult).toEqual({ ...foundMessage, mine: true });
+    });
+
+    it('modifyTime should call socketClientService.modifyTime', () => {
+        hintsDisplayService.modifyTime('classic');
+        expect(socketClientServiceSpy.modifyTime).toHaveBeenCalledWith('classic');
+    });
 });

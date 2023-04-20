2f8f77dcff1decb420944a85ea68b50995e531ee
Hamza Berrada <hamza.berrada@bell.ca>
2023-04-19 23:21:34 -0400

    HB: test start-hints

---
+++ client/src/app/classes/game-records/start-hints.spec.ts
@@ -0,0 +1,19 @@
+import { StartHintsRecord } from './start-hints';
+fdescribe('StartHints', () => {
+    let startHintsRecord: StartHintsRecord;
+
+    beforeEach(() => {
+        startHintsRecord = new StartHintsRecord(0);
+    });
+
+    it('should create an instance', () => {
+        expect(startHintsRecord).toBeTruthy();
+    });
+
+    it('should call showHints', () => {
+        const component = jasmine.createSpyObj('GamePageComponent', ['hintsService']);
+        component.hintsService = jasmine.createSpyObj('HintsService', ['showHints']);
+        startHintsRecord.do(component);
+        expect(component.hintsService.showHints).toHaveBeenCalled();
+    });
+});

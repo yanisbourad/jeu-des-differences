e51164163b1d2306b8ebb0cde5447ff5c70a6137
Daniel Giao <daniel.giao@icloud.com>
2023-04-20 15:36:19 -0400

    gamepage component hints fixed

--- client/src/app/pages/game-page/game-page.component.ts
+++ client/src/app/pages/game-page/game-page.component.ts
@@ -79,7 +79,13 @@ export class GamePageComponent implements OnInit, AfterViewInit, OnDestroy {
         this.gameService.handleDisconnect();
         if (this.gameService.mode === 'tempsLimite') {
             this.gameService.getTimeLimitGame();
-            if (this.gameService.gameType !== 'double') this.loadImages(this.socket.game);
+            if (this.gameService.gameType !== 'double') {
+                setTimeout(() => {
+                    this.cheatModeService.unfoundedDifference = this.gameService.getSetDifference(this.gameService.game.listDifferences);
+                    this.hintsService.unfoundedDifference = this.gameService.getSetDifference(this.gameService.game.listDifferences);
+                }, TIMEOUT * 3);
+                this.loadImages(this.socket.game);
+            }
             this.socket.imageLoaded$.subscribe((game: Game) => {
                 setTimeout(() => {
                     this.loadImages(game);
@@ -241,6 +247,7 @@ export class GamePageComponent implements OnInit, AfterViewInit, OnDestroy {
                 if (this.gameService.mode === 'tempsLimite') {
                     this.gameService.game = this.socket.getGame();
                     this.cheatModeService.unfoundedDifference = this.gameService.getSetDifference(this.gameService.game.listDifferences);
+                    this.hintsService.unfoundedDifference = this.gameService.getSetDifference(this.gameService.game.listDifferences);
                 }
             } else {
                 new ShowNotADiffRecord(canvases, this.gameService.mousePosition).record(this.gameRecordService);

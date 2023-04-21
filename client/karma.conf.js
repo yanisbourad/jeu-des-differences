269d7f1a47feb959ac01c38e295cc6e09f3d84da
Hamza Berrada <hamza.berrada@bell.ca>
2023-04-20 23:36:26 -0400

    HB: Test with normal chrome

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeNoAutoplay'],
+        browsers: ['Chrome'],
         singleRun: false,
         restartOnFileChange: true,
         customLaunchers: {
